49143e40eb4bd846f0217d4da3c3ca6068a9c52f
Dominik Guzei <dominik.guzei@gmail.com>
2013-10-01 11:05:05 +0200

    refactors injector specs to better structure

--- spec/agnostic/injection/Injector.spec.js
+++ spec/agnostic/injection/Injector.spec.js
@@ -4,27 +4,68 @@ var Injector = agnostic.injection.Injector;
 
 describe('agnostic.Injector:', function(){
 
+  beforeEach(function() {
+    this.TestInterface = Interface('TestInterface', {}, true);
+    this.TestClass = Class('TestClass', {}, true);
+
+    this.InjecteeClass1 = Class('InjecteeClass1', {
+      Dependencies: {
+        testInterfaceInstance: this.TestInterface
+      }
+    }, true);
+
+    this.InjecteeClass2 = Class('InjecteeClass2', {
+      Dependencies: {
+        testInterfaceInstance: this.TestInterface,
+        injectee1Instance: this.InjecteeClass1
+      }
+    }, true);
+
+    this.injector = new Injector();
+  });
+
+  it('handles multiple injection points correctly', function() {
+      
+    var injectee = new this.InjecteeClass2();
+
+    this.injector.map(this.TestInterface).toSingleton(this.TestClass);
+    this.injector.map(this.InjecteeClass1).toSingleton(this.InjecteeClass1);
+
+    this.injector.injectInto(injectee);
+
+    expect(injectee.testInterfaceInstance).to.be.instanceof(this.TestClass);
+    expect(injectee.injectee1Instance).to.be.instanceof(this.InjecteeClass1);
+
+  });
+
+  it('injects into mapped dependencies', function() {
+
+    var injectee = new this.InjecteeClass2();
+
+    this.injector.map(this.TestInterface).toSingleton(this.TestClass);
+    this.injector.map(this.InjecteeClass1).toSingleton(this.InjecteeClass1);
+
+    this.injector.injectInto(injectee);
+
+    expect(injectee.injectee1Instance.testInterfaceInstance).to.be.instanceof(this.TestClass);
+
+  });
+
+  it('throws an error when required dependency was not configured', function() {
+
+    var injectee = new this.InjecteeClass2(),
+        that = this;
+
+    this.injector.map(this.TestInterface).toSingleton(this.TestClass);
+
+    function injectWhileDependencyIsNotMapped() {
+      that.injector.injectInto(injectee);
+    }
+    
+    expect(injectWhileDependencyIsNotMapped).to.throw(Error);
+  });
+
   describe('Use Case: map to singleton,', function() {
-
-    beforeEach(function() {
-      this.TestInterface = Interface('TestInterface', {}, true);
-      this.TestClass = Class('TestClass', {}, true);
-
-      this.InjecteeClass1 = Class('InjecteeClass1', {
-        Dependencies: {
-          testInterfaceInstance: this.TestInterface
-        }
-      }, true);
-
-      this.InjecteeClass2 = Class('InjecteeClass2', {
-        Dependencies: {
-          testInterfaceInstance: this.TestInterface,
-          injectee1Instance: this.InjecteeClass1
-        }
-      }, true);
-
-      this.injector = new Injector();
-    });
 
     it('maps given interface to a single instance of given class', function() {
 
@@ -38,47 +79,6 @@ describe('agnostic.Injector:', function(){
       expect(injectee1.testInterfaceInstance).to.be.instanceof(this.TestClass);
       expect(injectee2.testInterfaceInstance).to.be.equal(injectee1.testInterfaceInstance);
 
-    });
-
-    it('handles multiple injection points correctly', function() {
-      
-      var injectee = new this.InjecteeClass2();
-
-      this.injector.map(this.TestInterface).toSingleton(this.TestClass);
-      this.injector.map(this.InjecteeClass1).toSingleton(this.InjecteeClass1);
-
-      this.injector.injectInto(injectee);
-
-      expect(injectee.testInterfaceInstance).to.be.instanceof(this.TestClass);
-      expect(injectee.injectee1Instance).to.be.instanceof(this.InjecteeClass1);
-
-    });
-
-    it('injects into mapped dependencies', function() {
-
-      var injectee = new this.InjecteeClass2();
-
-      this.injector.map(this.TestInterface).toSingleton(this.TestClass);
-      this.injector.map(this.InjecteeClass1).toSingleton(this.InjecteeClass1);
-
-      this.injector.injectInto(injectee);
-
-      expect(injectee.injectee1Instance.testInterfaceInstance).to.be.instanceof(this.TestClass);
-
-    });
-
-    it('throws an error when required dependency was not configured', function() {
-
-      var injectee = new this.InjecteeClass2(),
-          that = this;
-
-      this.injector.map(this.TestInterface).toSingleton(this.TestClass);
-
-      function injectWhileDependencyIsNotMapped() {
-        that.injector.injectInto(injectee);
-      }
-      
-      expect(injectWhileDependencyIsNotMapped).to.throw(Error);
     });
 
   });
